74554a3fd52c88af477105d6465e5b203b8ee6c8
Evangelos Michelioudakis <vagmcs@gmail.com>
2020-12-27 20:07:27 +0200

    More fonts

--- 8-fonts.sh
+++ 8-fonts.sh
@@ -25,8 +25,18 @@ echo "# INSTALLING FONTS - https://wiki.archlinux.org/index.php/Fonts"
 echo
 
 PACKAGES=(
-  ttf-hack
-  nerd-fonts-hack  # Patched font Hack from nerd-fonts library
+    adobe-source-code-pro-fonts  # Monospaced font for user interface and coding environments
+    nerd-fonts-fira-code         # Patched font Fira Code from the nerd-fonts library
+    nerd-fonts-hack              # Patched font Hack from nerd-fonts library
+    nerd-fonts-inconsolata       # Patched font In-consolata from the nerd-fonts library
+    nerd-fonts-jetbrains-mono    # Patched font JetBrains Mono from nerd-fonts library
+    nerd-fonts-source-code-pro   # Patched font SourceCodePro from nerd-fonts library
+    nerd-fonts-ubuntu-mono       # Patched font UbuntuMono from the nerd-fonts library
+    ttf-fira-code                # Monospaced font that includes programming ligatures
+    ttf-hack                     # A hand groomed and optically balanced typeface
+    ttf-inconsolata              # Monospace font for pretty code listings
+    ttf-jetbrains-mono           # Typeface for developers by JetBrains
+    ttf-ubuntu-font-family       # Ubuntu font family
 )
 
 for PKG in "${PACKAGES[@]}"; do
